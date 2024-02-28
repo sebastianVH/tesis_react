6e161a63347bc420a34bb089182e6964836650b6
luciacouti <maria.coutinho@davinci.edu.ar>
2024-02-28 01:06:18 -0300

    Ajuste estilo imagenes

--- front/src/MascotaListItem.jsx
+++ front/src/MascotaListItem.jsx
@@ -9,7 +9,7 @@ import { UserContext } from "./UserContext";
 function MascotaListItem({ mascota }) {
   const { userData } = useContext(UserContext);
   return (
-    <div className="col-xl-3 col-md-4 col-6 d-flex align-items-stretch px-2 px-md-3 px-xl-2 mb-4">
+    <div className="col-xl-3 col-md-4 col-6 align-items-stretch px-2 px-md-3 px-xl-2 mb-4">
       <Link to={`/mascotas/${mascota._id}`} className="mascota-view">
         <div className="card-perro">
           {mascota.aparecio && (
